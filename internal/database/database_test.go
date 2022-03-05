4c70f87c22b9cd77d7b59eb0020b2ad460e8d5a7
Donald Ball <donald.ball@gmail.com>
2022-03-05 17:01:18 -0500

    chore: specify identity uniqueness tempid behavior

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -34,3 +34,38 @@ func TestConnection(t *testing.T) {
 	assert.True(t, db1.Select(Selection{E: donald}).Next())
 	assert.False(t, db0.Select(Selection{E: donald}).Next())
 }
+
+func TestTempIDs(t *testing.T) {
+	t.Run("identity uniqueness resolves to an extant entity", func(t *testing.T) {
+		conn := OpenConnection()
+		txn, err := conn.Write(Request{
+			Claims: []Claim{
+				{E: TempID("name"), A: sys.DbIdent, V: String("person/name")},
+				{E: TempID("name"), A: sys.AttrType, V: sys.AttrTypeString},
+				{E: TempID("name"), A: sys.AttrUnique, V: sys.AttrUniqueIdentity},
+				{E: TempID("age"), A: sys.DbIdent, V: String("person/age")},
+				{E: TempID("age"), A: sys.AttrType, V: sys.AttrTypeInt},
+			}},
+		)
+		require.NoError(t, err)
+		name := txn.NewIDs[TempID("name")]
+		age := txn.NewIDs[TempID("age")]
+		txn, err = conn.Write(Request{
+			Claims: []Claim{
+				{E: TempID("me"), A: name, V: String("Donald")},
+				{E: TempID("me"), A: age, V: Int(7)},
+			},
+		})
+		require.NoError(t, err)
+		donald := txn.NewIDs[TempID("me")]
+		assert.NotEmpty(t, donald)
+		txn, err = conn.Write(Request{
+			Claims: []Claim{
+				{E: TempID("me"), A: name, V: String("Donald")},
+				{E: TempID("me"), A: age, V: Int(17)},
+			},
+		})
+		require.NoError(t, err)
+		assert.Equal(t, donald, txn.NewIDs[TempID("me")])
+	})
+}
